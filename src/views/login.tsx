24a6970fd4076313f9e9f3312fb0a9a82e96ebec
Krishna Sharma <Ks9192174414@gmail.com>
2023-09-20 00:10:19 +0530

    Complete the login page and also integrate it

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useForm } from '@mantine/form';
 import { Image, Box, TextInput, Button, Text, Divider } from '@mantine/core'
+import { userLogin } from '../services/user';
+
 export function Component() {
   return (
     <div className='mt-2 w-96 mx-auto'>
@@ -12,6 +15,25 @@ export function Component() {
 
 function LoginBox() {
   const navigate = useNavigate()
+  const form = useForm({
+    initialValues: { email: '', password: '' },
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      password: (value) => (value.length === 0 ? "Password cannot be empty" : null)
+    }
+  })
+
+  const handleSubmit = async () => {
+    const values = form.values;
+    const res = await userLogin({ email: values?.email, password: values?.password })
+    if (res.status === 200) {
+      localStorage.setItem('token', res.data?.authToken)
+      navigate('/')
+    } else {
+      console.log('Error')
+    }
+  }
+
   return (
     <Box
       sx={(theme) => ({
@@ -24,15 +46,17 @@ function LoginBox() {
     >
       <div>
         <div className='text-3xl mb-4'>Sign in</div>
-        <TextInput label="Enter the mobile phone number or email" />
-        <TextInput label="Password" />
-        <div className='mt-3'>
-          <Button variant='filled' className='w-full'>Continue</Button>
-        </div>
+        <form onSubmit={form.onSubmit(handleSubmit)}>
+          <TextInput label="Enter the mobile phone number or email" {...form.getInputProps('email')} />
+          <TextInput label="Password" {...form.getInputProps('password')} />
+          <div className='mt-3'>
+            <Button type='submit' variant='filled' className='w-full'>Continue</Button>
+          </div>
+        </form>
         <Text size='xs' className='mt-3 mb-4'>By Continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</Text>
       </div>
       <Divider label="New to Amazon?" labelPosition='center' />
       <Button variant='default' className='w-full mt-2' onClick={() => { navigate('/signup') }}>Create New Amazon Account</Button>
-    </Box>
+    </Box >
   );
 }
--- src/views/signup.tsx
+++ src/views/signup.tsx
@@ -1,39 +1,62 @@
-import React from "react"
-import { useForm } from '@mantine/form'
-import { useNavigate } from 'react-router-dom'
-import { Image, Box, TextInput, PasswordInput, Text, Button, Divider } from "@mantine/core";
-import { userCreate } from "../services/user";
+import React from 'react';
+import { useForm } from '@mantine/form';
+import { useNavigate } from 'react-router-dom';
+import {
+    Image,
+    Box,
+    TextInput,
+    PasswordInput,
+    Text,
+    Button,
+    Divider,
+} from '@mantine/core';
+import { userCreate } from '../services/user';
 
 export function Component() {
     return (
-        <div className='mt-2 w-96 mx-auto'>
-            <Image mx="auto" width={200} height={60} src="/images/amazon.svg" alt="amazon logo" />
+        <div className="mt-2 w-96 mx-auto">
+            <Image
+                mx="auto"
+                width={200}
+                height={60}
+                src="/images/amazon.svg"
+                alt="amazon logo"
+            />
             <SignUpBox />
         </div>
     );
 }
 
 function SignUpBox() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const form = useForm({
         initialValues: { name: '', email: '', phone: '', password: '' },
         validate: {
-            name: (value) => (value.length < 2 ? 'Name must have at least 2 letters' : null),
+            name: (value) =>
+                value.length < 2 ? 'Name must have at least 2 letters' : null,
             email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-            password: (value) => (value.length < 5 ? 'Password Must be at least of Length 5' : null)
-        }
-    })
+            password: (value) =>
+                value.length < 5 ? 'Password Must be at least of Length 5' : null,
+        },
+    });
 
     const handleSubmit = async () => {
-        console.log(form.values)
-        const values = form.values
-        const res = await userCreate({ name: values?.name || "", email: values?.email, mobile: values?.phone || "", password: values?.password })
+        console.log(form.values);
+        const values = form.values;
+        const res = await userCreate({
+            name: values?.name || '',
+            email: values?.email,
+            mobile: values?.phone || '',
+            password: values?.password,
+        });
         if (res.status === 200) {
-            console.log("user created success full")
+            localStorage.setItem('token', res?.data?.authToken);
+            navigate('/');
+            console.log('user created success full');
         } else {
-            console.log("error")
+            console.log('error');
         }
-    }
+    };
 
     return (
         <Box
@@ -41,22 +64,60 @@ function SignUpBox() {
                 padding: theme.spacing.xl,
                 borderRadius: theme.radius.md,
                 marginTop: '10px',
-                border: "1px solid gray",
+                border: '1px solid gray',
                 cursor: 'pointer',
             })}
         >
             <div>
-                <div className='text-3xl mb-4'> Create Account</div>
+                <div className="text-3xl mb-4"> Create Account</div>
                 <form onSubmit={form.onSubmit(handleSubmit)}>
-                    <TextInput name="name" label="Your name" placeholder="First and last name" className="mb-2" {...form.getInputProps('name')} />
-                    <TextInput name="phone" label="Mobile number" placeholder="Mobile Number" className="mb-2" />
-                    <TextInput name="email" label="Email(optional)" className="mb-2" {...form.getInputProps('email')} />
-                    <PasswordInput name="password" label="Password" placeholder="At least 5 characters" className="mb-2" {...form.getInputProps('password')} />
-                    <Text size='sm' className="mb-2">To verify your number, we will send you a text message with a temporary code. Message and data rates may apply.</Text>
-                    <Button type="submit" variant='filled' className='w-full mt-3'>Continue</Button>
+                    <TextInput
+                        name="name"
+                        label="Your name"
+                        placeholder="First and last name"
+                        className="mb-2"
+                        {...form.getInputProps('name')}
+                    />
+                    <TextInput
+                        name="phone"
+                        label="Mobile number"
+                        placeholder="Mobile Number"
+                        className="mb-2"
+                    />
+                    <TextInput
+                        name="email"
+                        label="Email(optional)"
+                        className="mb-2"
+                        {...form.getInputProps('email')}
+                    />
+                    <PasswordInput
+                        name="password"
+                        label="Password"
+                        placeholder="At least 5 characters"
+                        className="mb-2"
+                        {...form.getInputProps('password')}
+                    />
+                    <Text size="sm" className="mb-2">
+                        To verify your number, we will send you a text message with a
+                        temporary code. Message and data rates may apply.
+                    </Text>
+                    <Button type="submit" variant="filled" className="w-full mt-3">
+                        Continue
+                    </Button>
                 </form>
                 <Divider my="md" />
-                <Text size="sm">Already have an account? <Text component="a" c="blue" onClick={() => { navigate('/login') }}>Sign in</Text></Text>
+                <Text size="sm">
+                    Already have an account?{' '}
+                    <Text
+                        component="a"
+                        c="blue"
+                        onClick={() => {
+                            navigate('/login');
+                        }}
+                    >
+                        Sign in
+                    </Text>
+                </Text>
             </div>
         </Box>
     );
