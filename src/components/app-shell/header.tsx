1b4c965d0b95bbd26fae79a7ac26d3a532f4f590
Krishna Sharma <Ks9192174414@gmail.com>
2023-09-24 14:41:49 +0530

    complete the header

--- src/components/app-shell/header.tsx
+++ src/components/app-shell/header.tsx
@@ -10,7 +10,7 @@ export function MyHeader() {
     return (
         <div className="flex bg-black w-full h-13">
             {/* left header container */}
-            <div className="mt-2 flex">
+            <div className="mt-2 ml-4 flex">
                 <Image
                     src="/images/amazon_nav.svg"
                     className=""
@@ -21,14 +21,15 @@ export function MyHeader() {
                 <Button
                     variant="subtle"
                     leftIcon={<IconMapPin />}
-                    color="orange"
-                    className="ml-4"
+                    color='dark'
+                    className="ml-4 text-white font-bold"
                 >
                     Select Address
                 </Button>
             </div>
             <Select
-                className="mt-2 ml-4 w-2/5"
+                size='sm'
+                className="mt-2 ml-4 w-5/12"
                 rightSection={
                     // className = "bg-lightOrange w-full h-9 rounded-r-md"
                     <div className="bg-lightOrange w-full h-9 rounded-r-md">
@@ -38,6 +39,29 @@ export function MyHeader() {
                 placeholder="search Amazon.in"
                 data={genderOptions}
             />
-        </div>
+
+            <div className='ml-2 flex'>
+                <Button variant='subtle' color='dark' leftIcon={<Image src='/images/india.svg' width={18} />} className='text-white mt-2'>En</Button>
+                <Button variant='subtle' color='dark' className='text-white mt-2'>
+                    <div className='flex-col'>
+                        <label className='text-white font-normal'>Hello, sign in</label>
+                        <div className='font-bold'>Accounts & Lists</div>
+                    </div>
+                </Button>
+                <Button variant='subtle' color='dark' className='text-white mt-2'>
+                    <div className='flex-col'>
+                        <label className='text-white font-normal'>Returns</label>
+                        <div className='font-bold'>& Orders</div>
+                    </div>
+                </Button>
+                <Button variant='subtle' color='dark' className='text-white mt-2 pb-1' leftIcon={
+                    <div className='flex-col'>
+                        <div className='text-lightOrange text-sm font-semibold relative top-1 left-2'>0</div>
+                        <Image src="/images/cart.svg" width={20} />
+                    </div>
+                }><div className='mt-5 font-bold'>Cart</div></Button>
+            </div>
+
+        </div >
     );
 }
