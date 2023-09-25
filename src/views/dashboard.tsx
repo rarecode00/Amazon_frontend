f53aa5342c9f0497d3df0e8c4a275410c14ccecb
Krishna Sharma <Ks9192174414@gmail.com>
2023-09-25 23:58:44 +0530

    add the slider and working on home page

--- src/views/dashboard.tsx
+++ src/views/dashboard.tsx
@@ -1,17 +1,11 @@
+import { BackgroundImage } from '@mantine/core';
 import React from 'react'
 
-interface User {
-    name: string;
-}
-
 export function Component() {
-    let user: User | null = null
-    const userJson = localStorage.getItem('user');
-    if (userJson) user = JSON.parse(userJson)
     return (
         <>
-            <div>this is the dashboard</div>
-            <div>{user && user?.name}</div>
+            <BackgroundImage src='/images/amazon_slide.jpg' className='w-full p-0 h-full'>
+            </BackgroundImage>
         </>
     )
 }
