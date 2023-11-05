5fb18c11d540076ecdee05f3f6995a52d401e2d2
Krishna Sharma <Ks9192174414@gmail.com>
2023-11-05 21:26:29 +0530

    service for add product

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,5 +1,5 @@
 import instance from "../utils/AxiosInstace";
 
 export async function addProduct({ name, category, rating, price }: { name: string, category: string, rating: string, price: string }) {
-    return instance.post("/api/auth/createuser", { name, category, rating, price })
+    return instance.post("/api/product/add-product", { name, category, rating, price })
 }
